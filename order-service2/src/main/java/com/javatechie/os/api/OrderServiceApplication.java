c40d897649ebe12404a12f59b66c063a6b65988c
ShankarEngnr <88078431+ShankarEngnr@users.noreply.github.com>
2025-03-28 12:56:06 +0530

    New Author name

--- order-service2/src/main/java/com/javatechie/os/api/OrderServiceApplication.java
+++ order-service2/src/main/java/com/javatechie/os/api/OrderServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 /**
- * Author Sankar
+ * Author SankarGomathi
  */
 @SpringBootApplication
 @EnableEurekaClient
