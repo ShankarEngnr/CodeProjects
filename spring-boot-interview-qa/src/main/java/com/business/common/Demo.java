92cdf28792cd5bde3d4772a2e80cf2ed52497073
ShankarEngnr <shankarb.e172@gmail.com>
2025-07-09 11:32:06 +0530

    -springbootinterviewqa

--- spring-boot-interview-qa/src/main/java/com/business/common/Demo.java
+++ spring-boot-interview-qa/src/main/java/com/business/common/Demo.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Demo {
 
-	public Demo() {
-		System.out.println("Demo class scanned !!!!");
-	}
+    public Demo() {
+        System.out.println("Demo class scanned !!!!");
+    }
 }
